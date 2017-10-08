3a337c1ef52f6222952f42e5593dffd7d86b393c
nay <kunvunk@gmail.com>
2017-10-08 18:38:23 +0800

    fix php html loadhtml syntax error

--- example/translate_html.php
+++ example/translate_html.php
@@ -1,6 +1,8 @@
-<?php 
+<?php
+error_reporting(E_ALL);
 require_once 'bootstrap.php';
 
+//$html_content = file_get_contents('https://github.com/about');
 $html_content = file_get_contents('example.html');
 echo 'original:<br/>';
 echo $html_content;
--- src/Parser/HTMLParser.php
+++ src/Parser/HTMLParser.php
@@ -6,32 +6,33 @@ use Translation\Protocol\Parser;
 /**
  * Parse html,and pick content from it
  * use ParagraphParser to break the content from html into small sentences
- * 
+ *
  * html tag has attribute "notranslate" will be pass
  * @author Nay Kang
  *
  */
 class HTMLParser implements Parser{
-    
+
     protected $paragraphParser = null;
-    
+
     public function __construct(){
         $this->paragraphParser = new ParagraphParser();
     }
-    
+
     /**
-     * 
+     *
      * {@inheritDoc}
      * @see \Translation\Protocol\Parser::process()
      */
     public function process($html,callable $callback){
+        $html = $this->syntaxFix($html);
         $dom = new \DOMDocument();
         $dom->loadHTML($html);
         $elements = $dom->getElementsByTagName('html');
         $this->throughHTML($elements->item(0),$callback);
         return $dom->saveHTML();
     }
-    
+
     /**
      * Go through all html nodes,and pick content
      * @param \DOMElement $element
@@ -42,11 +43,11 @@ class HTMLParser implements Parser{
         foreach($element->childNodes as $item){
             if(in_array($item->nodeName, ['style','script']) || $item instanceof \DOMCdataSection)
                 continue;
-            
+
             if($item->attributes && $item->hasAttribute('notranslate')){
                 continue;
             }
-            
+
             if($item instanceof \DOMText && trim($item->nodeValue)){
                 $content = $item->nodeValue;
                 $content = $this->paragraphParser->process($content, $callback);
@@ -55,5 +56,20 @@ class HTMLParser implements Parser{
             }
             $this->throughHTML($item,$callback);
         }
+    }
+
+    /**
+    * Because the poor of php inner html parser,some spell mistake should fix
+    * eg: & => &amp;
+    * @param string $html
+    * @return string $html
+    */
+    public function syntaxFix($html){
+      //only under php7,symtax error will occur
+      //and javascript will be problem
+      if (version_compare(PHP_VERSION, '7.0.0') < 0){
+          $html = preg_replace('/\&(?!\w+;|#|&)/','&amp;',$html);
+      }
+      return $html;
     }
 }
